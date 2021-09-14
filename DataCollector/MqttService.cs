ff946e6029291d28a1c699e861fce2e779aafdda
Pavel Zaytsev <pavel@iberisoft.com>
2021-09-14 23:42:51 +0200

    Refactor startup code

---
+++ DataCollector/MqttService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using MongoDB.Bson;
+using MqttHelper;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    class MqttService : IHostedService
+    {
+        readonly Settings m_Settings;
+        readonly DbContext m_DbContext;
+        readonly NetClient m_NetClient;
+
+
+        public MqttService(Settings settings)
+        {
+            m_Settings = settings;
+            m_DbContext = new DbContext(Environment.GetEnvironmentVariable("DB_CONNECTION") ?? m_Settings.DbConnection);
+            m_NetClient = new NetClient(m_Settings.BaseTopic);
+            m_NetClient.MessageReceived += NetClient_MessageReceived;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await m_NetClient.StartAsync(Environment.GetEnvironmentVariable("BROKER_HOST") ?? m_Settings.BrokerHost);
+            await m_NetClient.SubscribeAsync("#");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await m_NetClient.StopAsync();
+        }
+
+        private void NetClient_MessageReceived(object sender, NetMessage e)
+        {
+            Console.WriteLine($"{e.Topic}: {e.Payload}");
+
+            var tokens = e.Topic.Split('/');
+            var message = new Message { DeviceId = tokens[0], Topic = tokens[1], Payload = BsonDocument.Parse(e.Payload) };
+            m_DbContext.Messages.InsertOne(message);
+        }
+    }
+}
--- DataCollector/Program.cs
+++ DataCollector/Program.cs
@@ -1,47 +1,24 @@
 ﻿using Microsoft.Extensions.Configuration;
-using MongoDB.Bson;
-using MqttHelper;
-using System;
-using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace DataCollector
 {
     static class Program
     {
-        static DbContext m_DbContext;
-
-        static async Task Main()
+        static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            var settings = config.Get<Settings>();
-
-            m_DbContext = new DbContext(Environment.GetEnvironmentVariable("DB_CONNECTION") ?? settings.DbConnection);
-
-            var netClient = new NetClient(settings.BaseTopic);
-            await netClient.StartAsync(Environment.GetEnvironmentVariable("BROKER_HOST") ?? settings.BrokerHost);
-            await netClient.SubscribeAsync("#");
-            netClient.MessageReceived += NetClient_MessageReceived;
-
-            if (!Console.IsInputRedirected)
-            {
-                Console.WriteLine("Press Enter to quit...");
-                Console.ReadLine();
-            }
-            else
-            {
-                await Task.Delay(-1);
-            }
-
-            await netClient.StopAsync();
+            CreateHostBuilder(args).Build().Run();
         }
 
-        private static void NetClient_MessageReceived(object sender, NetMessage e)
-        {
-            Console.WriteLine($"{e.Topic}: {e.Payload}");
-
-            var tokens = e.Topic.Split('/');
-            var message = new Message { DeviceId = tokens[0], Topic = tokens[1], Payload = BsonDocument.Parse(e.Payload) };
-            m_DbContext.Messages.InsertOne(message);
-        }
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostContext.Configuration))
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<MqttService>();
+                    services.AddSingleton(hostContext.Configuration.Get<Settings>());
+                });
     }
 }
